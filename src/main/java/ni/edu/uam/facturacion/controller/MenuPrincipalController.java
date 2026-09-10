570801556cc1b7f56f37183dcfa7315d5b74f1bc
CatilloZzz <jireyes@uamv.edu.ni>
2026-09-10 15:10:34 -0600

    Acfeat:aualización en el sistema de facturación

---
+++ src/main/java/ni/edu/uam/facturacion/controller/MenuPrincipalController.java
@@ -0,0 +1,30 @@
+package ni.edu.uam.facturacion.controller;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import ni.edu.uam.facturacion.util.SceneManager;
+import java.io.IOException;
+
+public class MenuPrincipalController {
+    @FXML
+    private void abrirProductos() {
+        try {
+            SceneManager.abrirVentana(
+                    "/ni/edu/uam/facturacion/fxml/producto-view.fxml",
+                    "Gestión de productos");
+        } catch (IOException e) {
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR,
+                    "No fue posible abrir Productos.").showAndWait();
+        }
+    }
+
+    @FXML
+    private void salir() {
+        Alert a = new Alert(Alert.AlertType.CONFIRMATION,
+                "¿Desea cerrar la aplicación?", ButtonType.OK, ButtonType.CANCEL);
+        if (a.showAndWait().orElse(ButtonType.CANCEL) == ButtonType.OK)
+            Platform.exit();
+    }
+}
